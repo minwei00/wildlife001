286909649ac65917a31b89853c725f47ecfc39a6
MINwei00 <minweiah585@gmail.com>
2026-05-24 06:58:20 +0800

    change path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   setLoading(true);
 
   try {
-    const res = await fetch("http://localhost:5000/api/chat", {
+    const res = await fetch("/api/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ message: userInput, userId: "guest_user" }),
